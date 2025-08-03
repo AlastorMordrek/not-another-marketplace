a41492e451bd47632540e4c4a73df19a13048ae7
AlastorMordrek <el.silas@pm.me>
2025-08-03 00:25:53 -0700

    First commit from my iMac workstation.

---
+++ src/main/java/com/silas_mordrek/not_another_marketplace/NotAnotherMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.silas_mordrek.not_another_marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotAnotherMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotAnotherMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silas_mordrek/not_another_marketplace/NotAnotherMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silas_mordrek.not_another_marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotAnotherMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
